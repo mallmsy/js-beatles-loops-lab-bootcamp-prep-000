dda172599c662c4a1e33a94124e1589305c7c003
Mallory Woods <malvwoods@gmail.com>
2019-02-17 16:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   let i = 0; i++;
   while (facts >= 0) {
-    return (`${facts[i]} '!!!`)
+    return (`${facts[i]} '!!!'`)
   }
 }
 
