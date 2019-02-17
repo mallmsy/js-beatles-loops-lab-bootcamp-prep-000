db7805cf01684838987bbed7d5dffd361e998aa3
Mallory Woods <malvwoods@gmail.com>
2019-02-17 17:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 function theBeatlesPlay(musicians, instruments) {
   const i = 0
-  for(let i = 0; i < theBeatlesPlay.length; i++);
+  for(let i = 0; i < theBeatlesPlay.length; i++) {
   return(`${musicians[i]} plays ${instruments[i]}`);
+  }
 }
 
 
