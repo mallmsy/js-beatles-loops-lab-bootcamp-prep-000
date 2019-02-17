4f078d55aad42746488a09d0b0ddfac9c7b435f6
Mallory Woods <malvwoods@gmail.com>
2019-02-17 16:23:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const i = 0
+  for(let i = 0; i < theBeatlesPlay.length; i++);
+  return(`${musicians[i]} plays ${instruments[i]}`);
+}
+
+function johnLennonFacts(facts) {
+  let i = 0; i++;
+  while (facts >= 0) {
+    return (`${facts[i]} '!!!`)
+  }
+}
+
+function iLoveTheBeatles(num) {
+  do {console.log("I love the Beatles!");
+  num++
+  }  while (num < 15)
+  }
